8508939d0a29694f0f8205b8594c9835482ce81c
TheHitMan7 <krtik.vrma@gmail.com>
2023-03-12 10:56:16 +0530

    Update Installer Revision
    Signed-off-by: TheHitMan7 <krtik.vrma@gmail.com>

--- envsetup.sh
+++ envsetup.sh
@@ -3,8 +3,8 @@
 # This file is part of The BiTGApps Project
 
 export VARIANT="$1"
-export RELEASE="v1.8"
-export VERSION='"v1.8"'
-export VERSIONCODE='"18"'
+export RELEASE="v1.9"
+export VERSION='"v1.9"'
+export VERSIONCODE='"19"'
 export BuildDate=`date +"%Y%m%d"`
-export BuildID="v1.8"
+export BuildID="v1.9"
